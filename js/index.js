f68b47b021fac9be4d7d880b7875972a97a40604
rakhmonov <bohodir29102001@gmail.com>
2022-09-28 22:02:26 +0500

    First commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// BURGER MENU FUNCTION
+const nav = document.getElementById("nav");
+const burger = document.getElementById("burger");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+// Universities slider
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 47,
+  // centeredSlides: true,
+  // fade: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
+
+// Second Slider
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 2,
+//   spaceBetween: 25,
+//   centeredSlides: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
