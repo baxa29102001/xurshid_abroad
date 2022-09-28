74479227ad27950da27110d8a28cc52d7df8314a
xurshidjonov <59279803+xurshidjonov@users.noreply.github.com>
2022-09-28 22:36:24 +0300

    karochi 40 boldi

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ var swiper = new Swiper(".mySwiper", {
 // Second Slider
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
-  spaceBetween: 25,
+  spaceBetween: 40,
   centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
